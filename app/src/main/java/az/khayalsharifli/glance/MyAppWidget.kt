c85ad0efbebb097228210bd0bca428bd64a60f4c
khayalsharifli <xeyalsherif@gmail.com>
2023-08-06 13:08:23 +0400

    first

---
+++ app/src/main/java/az/khayalsharifli/glance/MyAppWidget.kt
@@ -0,0 +1,62 @@
+package az.khayalsharifli.glance
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.Button
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+
+/* Import Glance Composables
+ In the event there is a name clash with the Compose classes of the same name,
+ you may rename the imports per https://kotlinlang.org/docs/packages.html#imports
+ using the `as` keyword.
+
+import androidx.glance.Button
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.text.Text
+*/
+class MyAppWidget : GlanceAppWidget() {
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        // Load data needed to render the AppWidget.
+        // Use `withContext` to switch to another thread for long running
+        // operations.
+
+        provideContent {
+            // create your AppWidget here
+            MyContent()
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
+            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(
+                    text = "Home",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/az/khayalsharifli/glance/MyAppWidgetReceiver.kt
@@ -0,0 +1,9 @@
+package az.khayalsharifli.glance
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class MyAppWidgetReceiver : GlanceAppWidgetReceiver() {
+    // Let MyAppWidgetReceiver know which GlanceAppWidget to use
+    override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
+}
